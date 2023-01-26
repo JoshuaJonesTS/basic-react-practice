809f98902957c9d60f15f51cda3ef431787b3fa3
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-26 13:10:53 -0800

    Create more headers

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
      <Header title={"First Title"}></Header>
+     <Header title={"Second Title"}></Header>
+     <Header title={"Third Title"}></Header>
+     <Header title={"Fourth Title"}></Header>
     </div>
   );
 }
