4199c4aeff3f0cf49bafd1e462b50251c860c203
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-26 13:25:33 -0800

    Create Square component

---
+++ src/blocks/Square/square.block.jsx
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+const Square = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Square;
