4888ba31ae98dceeb1c6e8e027f23b8035422bda
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-26 15:38:45 -0800

    Create file to hold photo object

---
+++ src/Photos/data.js
@@ -0,0 +1,23 @@
+// images
+import image from '../src/images/image-1.jpg'
+import image2 from '../src/images/image-2.jpg'
+import image3 from '../src/images/image-3.jpg'
+import image4 from '../src/images/image-4.jpg'
+
+// this object holds the string directories of the images
+const imageObj = [
+{
+    photo: image
+},
+{
+    photo: image2
+},
+{
+    photo: image3
+},
+{
+    photo: image4
+}
+];
+
+export default imageObj;
