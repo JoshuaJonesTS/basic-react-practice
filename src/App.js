17204b00bbfba8aa3dd7f9ad6cabc976fdd72010
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-26 13:18:55 -0800

    Create multiple border colors

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Header from './blocks/Header.block';
 function App() {
   return (
     <div className="App">
-     <Header title={"First Title"}></Header>
-     <Header title={"Second Title"}></Header>
-     <Header title={"Third Title"}></Header>
-     <Header title={"Fourth Title"}></Header>
+     <Header title={"First Title"} borderColor={"red"}></Header>
+     <Header title={"Second Title"} borderColor={"orange"}></Header>
+     <Header title={"Third Title"} borderColor={"yellow"}></Header>
+     <Header title={"Fourth Title"} borderColor={"green"}></Header>
     </div>
   );
 }
