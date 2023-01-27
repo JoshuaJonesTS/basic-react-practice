427fbd58f6774330cfbb80c9e70f60aba6ec292d
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-27 13:25:23 -0800

    Test CSS file
    fail

--- src/blocks/Square/square.block.jsx
+++ src/blocks/Square/square.block.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
-// import './square.styles.css'
+import './square.block.css'
 
 const Square = ({ picture, number, color }) => {
     let evenNumbers = number * 2;
     let word = `Number: ${evenNumbers}`
     return (
-        <div style={{
-            backgroundColor: color,
-            width: '15%',
-            height: '200px'
-            
-        }}>
+        <div className="test">
             <h1>{word}</h1>
             <img src={picture} alt=""></img>
         </div>
